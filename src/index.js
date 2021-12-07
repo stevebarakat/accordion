097a2085532de9c8311de708ab9546c279313366
Steve <stevebarakat@gmail.com>
2021-12-06 23:58:34 -0500

    consolidated state

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 import React, {useState, createContext, useContext} from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
-
-const data = [
-  {
-    label: 'Item One',
-    content: <Description content="itemOne" />,
-  },
-  {
-    label: 'Item Two',
-    content: <Description content="itemTwo" />,
-  },
-  {
-    label: 'Item Three',
-    content: <Description content="itemThree" />,
-  },
-];
+import initalState from './initialState';
 
 const AccordionContext = createContext();
 
@@ -34,15 +20,18 @@ function AccordionItem({children}) {
 }
 
 function AccordionButton({children}) {
-  const {index, activeIndex, setActiveIndex} = useContext(AccordionContext);
-  const isActive = index === activeIndex;
+  const {index, state, setState} = useContext(AccordionContext);
+  const isActive = index === state.activeIndex;
 
   return (
     <div
       data-panel-title
       className={isActive ? 'expanded' : ''}
       onClick={() => {
-        setActiveIndex(index);
+        setState({
+          ...state,
+          activeIndex: index,
+        });
       }}
     >
       {children}
@@ -51,8 +40,8 @@ function AccordionButton({children}) {
 }
 
 function AccordionPanel({children}) {
-  const {index, activeIndex} = useContext(AccordionContext);
-  const isActive = index === activeIndex;
+  const {index, state} = useContext(AccordionContext);
+  const isActive = index === state.activeIndex;
 
   return (
     <div data-panel-content className={isActive ? 'expanded' : ''}>
@@ -60,31 +49,20 @@ function AccordionPanel({children}) {
     </div>
   );
 }
-
-function Description({content}) {
-  const data = {
-    itemOne: "Item one's content.",
-    itemTwo: "Item two's content.",
-    itemThree: "Item three's content.",
-  };
-
-  return <div>{data[content]}</div>;
-}
-
 function App() {
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [state, setState] = useState(initalState);
 
   return (
     <div className="App">
       <Accordion>
-        {data.map((item, index) => (
+        {state.tasks.map((task, index) => (
           <AccordionContext.Provider
             key={index}
-            value={{index, activeIndex, setActiveIndex}}
+            value={{index, state, setState}}
           >
             <AccordionItem>
-              <AccordionButton>{item.label}</AccordionButton>
-              <AccordionPanel>{item.content}</AccordionPanel>
+              <AccordionButton>{task.text}</AccordionButton>
+              <AccordionPanel>Control Panel</AccordionPanel>
             </AccordionItem>
           </AccordionContext.Provider>
         ))}
---
+++ src/initialState.js
@@ -0,0 +1,42 @@
+const initialState = {
+  activeIndex: 0,
+  tasks: [
+    {
+      id: 'u34oi5u',
+      text: 'Task One',
+      completed: false,
+    },
+    {
+      id: 'k34h52l4',
+      text: 'Task Two',
+      completed: false,
+    },
+    {
+      id: '9cx4oi5u',
+      text: 'Task Three',
+      completed: false,
+    },
+    {
+      id: 'gdf46e4654j',
+      text: 'Task Four',
+      completed: false,
+    },
+    {
+      id: '41g9h6sfkj',
+      text: 'Task Five',
+      completed: false,
+    },
+    {
+      id: '34sd45665j',
+      text: 'Task Six',
+      completed: false,
+    },
+    {
+      id: 'sfsh45k34j',
+      text: 'Task Seven',
+      completed: false,
+    },
+  ],
+};
+
+export default initialState;
